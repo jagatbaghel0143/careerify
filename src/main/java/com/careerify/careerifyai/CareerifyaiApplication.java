bcfe95ae6acf0d4dfb69c7fd5f37ba0fd0efba47
Jagat Bhan Singh <jagatbaghel8979@gmail.com>
2024-12-17 15:41:11 +0530

    Setup Careerify Project

---
+++ src/main/java/com/careerify/careerifyai/CareerifyaiApplication.java
@@ -0,0 +1,13 @@
+package com.careerify.careerifyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerifyaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerifyaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerify/careerifyai/CareerifyaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerify.careerifyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerifyaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
